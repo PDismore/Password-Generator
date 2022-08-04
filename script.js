c3a9fce2f9f1f77f6b505681f323867e6de53b36
PDismore <pat.dismore@gmail.com>
2022-08-04 11:28:15 -0400

    setup and first variable additions

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  var userInput = window.prompt("How long do you want your password to be?")
+  var pwrodLength = parseInt(userInput)
+  console.log(userInput)
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
